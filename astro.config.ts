2b2891c0cf402bbe42d0fe79984cbda77646ed9c
RobertAKARobin <contact@robertakarobin.com>
2022-10-03 13:34:54 -0500

    egg

--- astro.config.ts
+++ astro.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
 				removeTagWhitespace: false,
 				sortAttributes: false,
 			},
+			svg: false, // Reenable once https://github.com/astro-community/astro-compress/issues/39 and greencutie.svg can be ignored
 		}),
 	],
 });
---
+++ src/js/konami.ts
@@ -0,0 +1,42 @@
+export const KONAMI__EVENT = `konami`;
+
+export const KonamiEvent = new Event(KONAMI__EVENT);
+
+const sequence = [
+	`ArrowUp`,
+	`ArrowUp`,
+	`ArrowDown`,
+	`ArrowDown`,
+	`ArrowLeft`,
+	`ArrowRight`,
+	`ArrowLeft`,
+	`ArrowRight`,
+	`KeyB`,
+	`KeyA`,
+];
+
+export const state = {
+	input: [],
+};
+
+
+function konamiListener(event: KeyboardEvent) {
+	state.input.push(event.code);
+
+	const inputString = state.input.join(`.`);
+	const sequenceString = sequence.join(`.`);
+
+	if (!sequenceString.startsWith(inputString)) {
+		state.input = [];
+		return;
+	}
+
+	if (sequenceString === inputString) {
+		state.input = [];
+		window.dispatchEvent(KonamiEvent);
+	}
+}
+
+export function konamiSetup() {
+	window.addEventListener(`keyup`, konamiListener);
+}
