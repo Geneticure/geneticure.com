13ef4461fde1a442b9939e2a4267f61273fac39f
RobertAKARobin <contact@robertakarobin.com>
2022-10-03 09:37:14 -0500

    Use intersectionObserver for isInViewport

--- src/js/isInViewport.ts
+++ src/js/isInViewport.ts
@@ -1,50 +1,29 @@
-import { throttle } from 'src/js/timer';
-
 export class IsInViewport {
 	static defaultOptions = {
 		callback: null as (options: {
 			$element: Element;
 			isInViewport: boolean;
 		}) => void,
-		eventNames: [`load`, `orientationChange`, `resize`, `scroll`] as const,
 		getElements: null as () => Array<Element>,
-		throttleMs: 200 as number,
 	} as const;
 
-	/**
-	 * Note: Only tests vertically, not horizontally.
-	 */
-	// TODO3: Use IntersectionObserver
-	static test($element: Element) {
-		const boundaries = $element.getBoundingClientRect();
-		const maxHeight = window.innerHeight;
-		return (
-			(boundaries.top >= 0 && boundaries.top < maxHeight) ||
-			(boundaries.bottom >= 0 && boundaries.bottom < maxHeight) ||
-			(maxHeight > boundaries.top && maxHeight < boundaries.bottom)
-		);
-	}
-
-	test: (...args: Array<unknown>) => unknown;
+	observer: IntersectionObserver;
 
 	constructor(options: Partial<typeof IsInViewport[`defaultOptions`]> = {}) {
-		const eventNames = options.eventNames ?? IsInViewport.defaultOptions.eventNames;
-		const throttleMs = options.throttleMs ?? IsInViewport.defaultOptions.throttleMs;
-
-		this.test = () => {
-			const $elements = Array.from(options.getElements());
-			for (const $element of $elements) {
+		this.observer = new IntersectionObserver((entries) => {
+			for (const entry of entries) {
+				const $element = entry.target as HTMLElement;
 				options.callback({
 					$element,
-					isInViewport: IsInViewport.test($element),
+					isInViewport: entry.isIntersecting,
 				});
 			}
-		};
-
-		const testThrottled = throttle({ callback: this.test, interval: throttleMs });
-		eventNames.forEach((eventName) => {
-			window.addEventListener(eventName, testThrottled);
 		});
+
+		const $elements = options.getElements();
+		for (const $element of $elements) {
+			this.observer.observe($element);
+		}
 	}
 }
 
@@ -52,7 +31,7 @@ export function isInViewportSetup() {
 	const ATTR__VIEWPORT_VISIBLE = `data-viewport-visible`;
 	const $targets = Array.from(document.querySelectorAll(`[${ATTR__VIEWPORT_VISIBLE}]`));
 
-	const viewportListener = new IsInViewport({
+	new IsInViewport({
 		callback: (({ $element, isInViewport }) => {
 			if (isInViewport) {
 				$element.setAttribute(ATTR__VIEWPORT_VISIBLE, `true`);
@@ -62,5 +41,4 @@ export function isInViewportSetup() {
 		}),
 		getElements: () => $targets,
 	});
-	viewportListener.test();
 }
