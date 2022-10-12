5729d538774ec18b9498353f797a6a831159388e
RobertAKARobin <contact@robertakarobin.com>
2022-10-12 15:42:59 -0500

    add fb pixel

---
+++ public/js/tracking.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import 'https://www.googletagmanager.com/gtag/js?id=UA-58090183-1';
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-58090183-1');
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1300441860085124');
+fbq('track', 'PageView');
